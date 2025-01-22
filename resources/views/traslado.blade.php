94b78e4daf52210de4f48a6c026434b93fa9dd5a
L <garithos2ez4me@gmail.com>
2025-01-22 17:00:14 -0500

    agregado el modal de confirmación

--- resources/views/traslado.blade.php
+++ resources/views/traslado.blade.php
@@ -3,10 +3,59 @@
 @section('title', 'Traslados')
 
 @section('content')
-<div class="container">
-    <div class="bg-secondary" id="hidden-body" style="position:fixed;left:0;width:100vw;height:100vh;z-index:998;opacity:0.5;display:none">
+
+<style>
+    .modal {
+    display: none;
+    position: fixed;
+    z-index: 9999; 
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4); 
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    max-width: 400px;
+    text-align: center;
+    z-index: 10000; 
+}
+
+button {
+    padding: 10px 20px;
+    margin: 5px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+.btn-confirm {
+    background-color: green;
+    color: white;
+}
+
+.btn-cancel {
+    background-color: red;
+    color: white;
+}
+</style>
+
+<div id="hidden-body" style="position:fixed;left:0;width:100vw;height:100vh;z-index:998;opacity:0.5;display:none"></div>
+
+<div id="modalConfirmacion" class="modal">
+    <div class="modal-content">
+        <p>¿Estás seguro de que deseas proceder con la reubicación?</p>
+        <button id="btn-confirmar" class="btn-confirm">Confirmar</button>
+        <button id="btn-cancelar" class="btn-cancel">Cancelar</button>
     </div>
-    <br>
+</div>
+
+<div class="container">
     <div class="row">
         <div class="col-5 col-md-7">
             <h2><a href="{{route('documentos', [now()->format('Y-m')])}}" class="text-secondary"><i class="bi bi-arrow-left-circle"></i></a> Traslados</h2>
@@ -37,7 +86,7 @@
         <form action="{{route('updateregistroalmacen')}}" method="post">
             @csrf
             <div class="col-12">
-                <ul class="list-group" id="lista-traslado" style="visibility: hidden; overflow: auto; po">
+                <ul class="list-group" id="lista-traslado" style="visibility: hidden; overflow: auto;">
                     <li class="list-group-item bg-sistema-uno text-light">
                         <div class="row pt-1 text-center">
                             <div class="col-md-4">
@@ -61,11 +110,13 @@
                 </ul>
             </div>
             <br>
+            
             <div class="col-md-12 text-center" id="btn-reubicar" style="display: none">
-                <button type="submit" class="btn btn-success"><i class="bi bi-arrow-left-right"></i> Reubicar</button>
+                <button type="submit" class="btn btn-success" id="btn-reubicar-submit" disabled><i class="bi bi-arrow-left-right"></i> Reubicar</button>
             </div>
         </form>
     </div>
+    
 
     <div class="row" id="aviso-vacio">
         <div class="col-12 d-flex justify-content-center align-items-center text-secondary text-decoration-underline" style="height:60vh">
@@ -73,6 +124,7 @@
         </div>
     </div>
 </div>
+
 <script>
     var almacenes = @json($almacenes);
 </script>
